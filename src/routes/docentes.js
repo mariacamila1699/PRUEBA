b87dc0bc051009db9e27bf81433ae4a4432ceb6a
Camila Nuñez <camilanunez.1606@gmail.com>
2020-04-02 09:32:10 -0500

    Update docentes.js

--- src/routes/docentes.js
+++ src/routes/docentes.js
@@ -19,15 +19,17 @@ router.post('/agregar', async(req,res) => {
 });
 
 
-router.get('/consultar',(req,res) => {
+router.get('/consultar', async(req,res) => {
+    const docentes = await pool.query('select * from docentes');
     //res.send('oy yeah');
-    res.render('docentes/consultar');
+    res.render('docentes/consultar',{docentes});
+    
 });
 
 router.get('/eliminar/:id_docente',async(req,res) => {
     const {id_docente} = req.params;
     const docente = await pool.query('delete from docentes where id_docente=?',[id_docente]);
-    res.redirect('/docentes/agregar');
+    res.redirect('/docentes/consultar');
 });
 
 router.get('/modificar',(req,res) => {
